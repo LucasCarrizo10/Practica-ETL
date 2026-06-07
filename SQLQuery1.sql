eff98105010912570e6da64815995d83cc357e11
LucasCarrizo10 <lpcda2022@gmail.com>
2026-06-07 19:46:02 -0300

    SQLQuery
    Script hecho en SQL Server para crear la tabla "Customers"

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+USE Supermercado;
+GO
+
+CREATE TABLE dbo.Customers (
+    CustomerID nchar(5) PRIMARY KEY,
+    CompanyName nvarchar(40) NOT NULL,
+    ContactName nvarchar(30) NULL,
+    ContactTitle nvarchar(30) NULL,
+    Address nvarchar(60) NULL,
+    City nvarchar(50) NULL,
+    Region nvarchar(15) NULL,
+    PostalCode nvarchar(10) NULL,
+    Country nvarchar(15) NULL,
+    Phone nvarchar(50) NULL,
+    Fax nvarchar(50) NULL
+);
+GO
